893456b6873f9bafd1ab46457bf3322202590972
lauramgll <156073035+lauramgll@users.noreply.github.com>
2024-12-12 12:43:39 +0100

    Testing login

--- spotify/src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ spotify/src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -3,6 +3,7 @@ import { LoginPageComponent } from './login-page.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -26,5 +27,53 @@ describe('LoginPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Debe de asegurarse de que el formulario sea inválido cuando ingrese datos erróneos
+  // Patrón AAA
+  it('Debería retornar inválido el formulario', () => {
+
+    // Arrange
+    const mockCredentials = {
+      email: '0x0x0x0x0',
+      password: '111111111111111'
+    }
+
+    const emailForm: any = component.formLogin.get('email');
+    const passwordForm: any = component.formLogin.get('password');
+
+    // Act
+    emailForm.setValue(mockCredentials.email);
+    passwordForm.setValue(mockCredentials.password);
+
+    // Assert (qué esperamos)
+    expect(component.formLogin.invalid).toBeTrue();
+  });
+
+  // Debe de asegurarse de que el formulario sea válido
+  it('Debería retornar válido el formulario', () => {
+
+    // Arrange
+    const mockCredentials = {
+      email: 'test@test.com',
+      password: '12345678'
+    }
+
+    const emailForm: any = component.formLogin.get('email');
+    const passwordForm: any = component.formLogin.get('password');
+
+    // Act
+    emailForm.setValue(mockCredentials.email);
+    passwordForm.setValue(mockCredentials.password);
+
+    // Assert (qué esperamos)
+    expect(component.formLogin.invalid).toBeFalse();
+  });
+
+  it('El botón debería de tener la palabra "Iniciar sesión"', () => {
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'));
+    const getInnerText = elementRef.nativeElement.innerText;
+
+    expect(getInnerText).toEqual('Iniciar sesión');
   });
 });
